2ec85ad8339cdc18176c3f10903a4d8432f1d94d
james robert williams & Alec Arme <student@epicodus.com>
2015-04-30 09:32:30 -0700

    Add spec for #last_name. Passes spec.

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -6,14 +6,14 @@ describe(Contact) do
 
   describe("first_name") do
     it('provides the first name of the contact') do
-      test_contact = Contact.new(first_name: 'james', last_name: 'williams')
+      test_contact = Contact.new(:first_name 'james', :last_name 'williams')
       expect(test_contact.first_name()).to(eq('james'))
     end
   end
 
   describe("last_name") do
     it('provides the last name of the contact') do
-      test_contact = Contact.new(first_name: "Chuck-e", last_name: 'Cheese')
+      test_contact = Contact.new(:first_name "Chuck-e", :last_name 'Cheese')
       expect(test_contact.last_name()).to(eq('Cheese'))
     end
   end
